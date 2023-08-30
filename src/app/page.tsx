bb007ba9e736d2cd5ba96f57e089397f9e6a4b80
Yuri Garrido <y.garrido.yuri@gmail.com>
2023-08-30 10:02:45 -0300

    feat: add tabs with radix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
-import Image from 'next/image'
+import { SettingsTabs } from '@/components/SettingsTabs'
 
 export default function Home() {
-  return <h1>Hello World</h1>
+  return (
+    <>
+      <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
+
+      <SettingsTabs />
+    </>
+  )
 }
